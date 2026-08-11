6a2c03c5e17087a8e3dacdb4be7babbee019e1c5
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:11:13 +0800

    Add stub: RevenueVolumePage

---
+++ src/pages/RevenueVolumePage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function RevenuePage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Total Revenue Volume</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
