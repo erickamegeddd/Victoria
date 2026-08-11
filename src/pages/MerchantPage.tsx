e02e1cb3f159d07d887cc89dff6512d774cb27e4
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:11:45 +0800

    Add stub: MerchantPage

---
+++ src/pages/MerchantPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function MerchantPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Merchant Detail</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
