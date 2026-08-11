ef9490b20dcc4892c0e30b1b06aab23a4f69ceee
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:11:48 +0800

    Add stub: PaymentsPage

---
+++ src/pages/PaymentsPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function Payments() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Payments</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
