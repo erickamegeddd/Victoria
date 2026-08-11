1d30f7f2b0e8c68c199b67519cae0097859579c5
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:09 +0800

    Add stub: iso/MidPerIsoPage

---
+++ src/pages/iso/MidPerIsoPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function MidPerIso() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>MIDs Per ISO</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
