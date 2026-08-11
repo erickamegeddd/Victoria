6ab6deedc95a1aeaead62811f67106bdd71893d7
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:35 +0800

    Add stub: agentsPages/MonthlyDataPage

---
+++ src/pages/agentsPages/MonthlyDataPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function MonthlyData() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Monthly Data</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
