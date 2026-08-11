b60381059010f7c276d5321454edb0cc1087efbe
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:01 +0800

    Add stub: iso/AddIsoPage

---
+++ src/pages/iso/AddIsoPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function AddIsoPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Add ISO</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
