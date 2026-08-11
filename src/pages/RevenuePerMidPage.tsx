cde98a3dae934bc40641c9d669d9833e782dbd8a
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:11:17 +0800

    Add stub: RevenuePerMidPage

---
+++ src/pages/RevenuePerMidPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function RevenuePerMidPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Revenue Per MID</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
