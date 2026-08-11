7d5e26166e0037f8c19799b9e73f4ea1a2fd9790
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:52 +0800

    Add stub: users/ViewUsersPage

---
+++ src/pages/users/ViewUsersPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function ViewUsersPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>View Users</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
