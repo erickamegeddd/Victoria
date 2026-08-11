6b9c78fc52d337422e9f9e7ff3076d993dac45eb
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:26 +0800

    Add stub: adjustments/ViewAdjustments

---
+++ src/pages/adjustments/ViewAdjustments.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function ViewAdjustments() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>View Adjustments</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
