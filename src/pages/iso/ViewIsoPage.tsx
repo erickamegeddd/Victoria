ea4a1f7689e3668ce8ea97fa11bd5bd41025588c
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:05 +0800

    Add stub: iso/ViewIsoPage

---
+++ src/pages/iso/ViewIsoPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function ViewIsoPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>View ISO</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
