56d1e53f1d282f984bb6df19e9e280b3e8d62658
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:12:13 +0800

    Add stub: adjustments/AddAdjustments

---
+++ src/pages/adjustments/AddAdjustments.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function AddAdjustments() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Add Adjustments</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
