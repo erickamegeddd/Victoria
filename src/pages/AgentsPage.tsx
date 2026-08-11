88ccda2dca54f68f6b4129e6446d18aa773be01a
erickameged <erickameged.thevagroup@gmail.com>
2026-08-12 00:11:20 +0800

    Add stub: AgentsPage

---
+++ src/pages/AgentsPage.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+export default function AgentsPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Agents Data</h2>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
