ec4571754bd0077ef45f6918b93ac7b8209e089f
erickameged <erickameged.thevagroup@gmail.com>
2026-08-11 22:25:45 +0800

    Add AgentsPayoutBarChart.tsx

---
+++ src/components/charts/AgentsPayoutBarChart.tsx
@@ -0,0 +1,86 @@
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+} from "recharts";
+import client from "../../utils/axios";
+import { useQuery } from "react-query";
+import { Col, Empty, message, Row } from "antd";
+
+interface AgentsPayoutBarChartProps {
+  date: string | string[];
+}
+
+const AgentsPayoutBarChart: React.FC<AgentsPayoutBarChartProps> = ({
+  date,
+}) => {
+  const fetchAgentsPayout = async (date: string | string[]) => {
+    const { data } = await client.get(`/agents-payout`, {
+      params: { date },
+    });
+    return data;
+  };
+
+  const { data, error } = useQuery(
+    ["fetchAgentsPayout", date],
+    () => fetchAgentsPayout(date),
+    {
+      enabled: !!date,
+    }
+  );
+
+  if (error) {
+    message.error("Error Fetching data");
+  }
+
+  return (
+    <>
+      <Row justify="center">
+        <Col>
+          {data.length > 0 ? (
+            <ResponsiveContainer width={1000} height={500}>
+              <BarChart
+                data={data}
+                layout="vertical" // Set the chart to be horizontal
+                margin={{
+                  top: 20,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <YAxis type="category" dataKey="agent_name" width={150} />
+                <XAxis
+                  type="number"
+                  tickFormatter={(value) => `$${value.toLocaleString()}`}
+                />
+                <Tooltip
+                  formatter={(value) => [
+                    `$${value.toLocaleString()}`,
+                    "Total Payout",
+                  ]}
+                />
+                <Legend />
+                <Bar
+                  dataKey="total_payout"
+                  fill="var(--secondary-color)"
+                  name="Total Payout"
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          ) : (
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          )}
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default AgentsPayoutBarChart;
